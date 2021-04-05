f31ce013d7a78006f87e281d0d4079c78e9466c1
Ohunter <ohoffs@gmail.com>
2021-04-05 14:49:56 +0200

    removed stuff left after debugging

--- examples/tls/0/main.cpp
+++ examples/tls/0/main.cpp
@@ -51,43 +51,41 @@ int main(int argc, char *argv[]) {
     // Start server
     std::thread t0(server, "127.0.0.1", 54321);
 
-    if (argc > 1) {
-        // Sleep for 1 second to let the server get to the `accept` call
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+    // Sleep for 1 second to let the server get to the `accept` call
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 
-        // Initialize the SSL context
-        SSL_CTX *ctx = setup_client_ctx();
+    // Initialize the SSL context
+    SSL_CTX *ctx = setup_client_ctx();
 
-        char        buf[256] = {0};
-        std::string s        = "";
+    char        buf[256] = {0};
+    std::string s        = "";
 
-        try {
-            // Connect to the server
-            auto sock = Sockets::TLSSocket::Connect("127.0.0.1", 54321, ctx,
-                                                    Sockets::Domain::IPv4);
+    try {
+        // Connect to the server
+        auto sock = Sockets::TLSSocket::Connect("127.0.0.1", 54321, ctx,
+                                                Sockets::Domain::IPv4);
 
-            std::cout << "Enter the message: \n";
+        std::cout << "Enter the message: \n";
 
-            std::getline(std::cin, s);
-            char b[] = {(char)s.size(), 0};
-            sock.send(b, 1);
-            sock.send(s.c_str(), s.size());
+        std::getline(std::cin, s);
+        char b[] = {(char)s.size(), 0};
+        sock.send(b, 1);
+        sock.send(s.c_str(), s.size());
 
-            sock.recv(b, 1);
-            sock.recv(buf, (int)b[0]);
+        sock.recv(b, 1);
+        sock.recv(buf, (int)b[0]);
 
-            std::cout << "Client recieved " << (int)b[0]
-                      << " bytes containing: " << std::string(buf) << std::endl;
+        std::cout << "Client recieved " << (int)b[0]
+                  << " bytes containing: " << std::string(buf) << std::endl;
 
-            // Close the connection
-            sock.close();
-        } catch (const std::exception &e) {
-            std::cerr << e.what() << std::endl;
-            ERR_print_errors_fp(stderr);
-        }
-
-        SSL_CTX_free(ctx);
+        // Close the connection
+        sock.close();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        ERR_print_errors_fp(stderr);
     }
+
+    SSL_CTX_free(ctx);
 
     t0.join();
 
